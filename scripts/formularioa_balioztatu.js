baaa1e535897953cf3cb3185ffcd2c27172446b6
Julen <larraagirrejulen@gmail.com>
2021-11-08 13:45:17 +0100

    responsive formularioa

--- scripts/formularioa_balioztatu.js
+++ scripts/formularioa_balioztatu.js
@@ -1,14 +1,13 @@
 function formularioa_balioztatu(){
-	formulario_colorea_eguneratu();
 	izena = $("#izena").val();
 	posta = $("#posta").val();
 	iruzkina = $("#iruzkina").val();
+	
 	if(izena.length > 0){
 		if(iruzkina.length > 0){
 			if(posta.length > 0){
-				if(!posta_balioztatu(posta)){
-					document.getElementById("posta").style.borderColor = "red";
-					alert("Posta desegokia!");
+				if(!posta_balioztatu()){
+						alert("Posta desegokia!");
 				}else{
 					return true;
 				}
@@ -26,19 +25,43 @@ function formularioa_balioztatu(){
 	return false;
 }
 
-function posta_balioztatu(posta){
+function iruzkina_balioztatu(){
+	iruzkina = $("#iruzkina").val();
+	if(iruzkina.length > 0){
+		document.getElementById("iruzkina").style.borderColor = "black";
+	}else{
+		document.getElementById("iruzkina").style.borderColor = "red";
+	}
+}
+
+function izena_balioztatu(){
+	izena = $("#izena").val();
+	if(izena.length > 0){
+		document.getElementById("izena").style.borderColor = "black";
+	}else{
+		document.getElementById("izena").style.borderColor = "red";
+	}
+}
+
+function posta_balioztatu(){
+	posta = $("#posta").val();
 	index = posta.indexOf("@");
 	if(index > 0 && posta.indexOf("@", index + 1) == -1){
 		pointIndex = posta.lastIndexOf(".");
 		if(pointIndex - 1 > index && posta.length - pointIndex > 2){
+			document.getElementById("publikoa").style.display = 'inline-block';
+			document.getElementById("lbl_publikoa").style.display = 'inline-block';
+			document.getElementById("posta").style.borderColor = "green";
 			return true;
 		}
 	}
-	return false;
-}
-
-function formulario_colorea_eguneratu(){
-	document.getElementById("iruzkina").style.borderColor = "black";
-	document.getElementById("izena").style.borderColor = "black";
-	document.getElementById("posta").style.borderColor = "black";
+	if(posta.length > 0){
+		document.getElementById("publikoa").style.display = 'none';
+		document.getElementById("lbl_publikoa").style.display = 'none';
+		document.getElementById("posta").style.borderColor = "red";
+		return false;
+	}else{
+		document.getElementById("posta").style.borderColor = "black";
+		return true;
+	}
 }
