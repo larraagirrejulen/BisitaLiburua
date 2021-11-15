52ba82c823f323da426bc213cc14b277d5641663
Julen <larraagirrejulen@gmail.com>
2021-11-15 13:10:56 +0100

    erabiltzaile zehatz baten iruzkinak ikuskatzeko taula aldatu

--- php/iruzkinak_ikusi.php
+++ php/iruzkinak_ikusi.php
@@ -60,19 +60,21 @@
 
 					<input type='submit' name='bidali' id='bidali' value='Erabiltzailearen iruzkinak ikusi'>
 				</form><br>
+				<table class='erabiltzaile_taula'>
+					<thead>
+						<tr>
+							<th>Data</th><th>Posta</th><th>Iruzkina</th>
+						</tr>
+					</thead>
+					<tbody>
 				<?php
 					if(isset($_POST['izena'])){
-						echo "<table class='erabiltzaile_taula'>
-									<thead>
-										<tr>
-											<th>Data</th><th>Posta</th><th>Iruzkina</th>
-										</tr>
-									</thead>
-									<tbody> ";
 						$bisitak = simplexml_load_file('../xml/bisita_liburua.xml');
+						$aurkitua = 0;
 						foreach ($bisitak-> children() as $bisita){
 							$izena = $bisita[0]->izena[0];
 							if($izena == $_POST['izena']){
+								$aurkitua = 1;
 								echo "<tr>";
 
 								$data = $bisita[0]->data[0];
@@ -89,10 +91,15 @@
 								echo "</tr>";
 							}
 						}
-						echo "</tbody>
-									</table>";
+						if (!$aurkitua) {
+							echo "<tr><td>-</td><td>-</td><td>-</td></tr>";
+						}
+					}else{
+						echo "<tr><td>-</td><td>-</td><td>-</td></tr>";
 					}
 				 ?>
+				 </tbody>
+			</table>
 			</div>
 		</section>
 	</body>
